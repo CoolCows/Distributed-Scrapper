506e5d0b2f001f94cb81bbc71dcbb59ef094d57e
RodroVMS <democlesvanshepars@gmail.com>
2021-06-16 16:46:06 -0400

    Fix bugs

--- scrap_chord/scrap_chord_client.py
+++ scrap_chord/scrap_chord_client.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from scrap_chord.util import in_between
 import sys
 import pickle
@@ -9,7 +10,7 @@ from utils.const import CHORD_BEACON_PORT, CODE_WORD_CHORD, REP_CLIENT_INFO, REP
 import zmq.sugar as zmq
 from sortedcontainers import SortedSet
 
-from utils.tools import connect_router, find_nodes, get_id, get_router, get_source_ip, parse_address, register_socks, zpipe
+from utils.tools import address_to_string, connect_router, find_nodes, get_id, get_router, get_source_ip, parse_address, register_socks, zpipe
 
 
 class ScrapChordClient:
@@ -17,14 +18,13 @@ class ScrapChordClient:
         self.address = (get_source_ip(), port)
         self.context = zmq.Context()
         self.usr_send_pipe = zpipe(self.context)
-        self.recv_send_pipe = zpipe(self.context)
 
         self.bits = m
         self.recv_cache = dict()
         self.online = True
 
         # debbug & info
-        logging.basicConfig(format = "%(name)s: %(levelname)s: %(message)s", level=logging.INFO)
+        logging.basicConfig(format = "%(name)s: %(levelname)s: %(message)s", level=logging.DEBUG)
         self.logger = logging.getLogger("client")
 
     def run(self, address:str = ""):
@@ -35,10 +35,12 @@ class ScrapChordClient:
             self.logger.info("No adress specified, finding chords online ...")
             self.add_node(known_nodes, *self.get_chord_nodes())
             if len(known_nodes) == 0:
-                self.logger.info("No available chord nodes found. Exiting ...")
+                self.logger.info("No available chord nodes found. Exiting.")
                 return
         else:
-            self.add_node(known_nodes, parse_address(address))
+            address = parse_address(address)
+            address = (address[0], address[1] + 1)
+            self.add_node(known_nodes, address)
         
         t = Thread(target=self.communicate_with_chord, args=(known_nodes,), daemon=True)
         t.start()
@@ -53,23 +55,27 @@ class ScrapChordClient:
                 self.logger.info(f"Recieved {url}\n {html[:100]} \n ...") # Print url and first 100 chars from html
             if sys.stdin.fileno() in socks:
                 for line in sys.stdin:
+                    self.logger.debug(f"Sending to pyobj: {line.split()}")
                     self.usr_send_pipe[0].send_pyobj(line.split())
                     break
 
     def communicate_with_chord(self, known_nodes:SortedSet):
         comm_sock = get_router(self.context)
         
+        connected = set()
         pending_recv = SortedSet()
         poller = zmq.Poller()
         register_socks(
-            poller, self.recv_send_pipe[1], self.usr_send_pipe[1]
+            poller, comm_sock, self.usr_send_pipe[1]
         )
         while True:
-            socks = dict(poller.poll(1000))
+            socks = dict(poller.poll(1500))
             if self.usr_send_pipe[1] in socks:
-                url_list:Tuple = self.recv_send_pipe[1].recv_pyobj()
+                url_list:Tuple = self.usr_send_pipe[1].recv_pyobj()
+                self.logger.debug(f"Recieving usr request: {url_list}")
             elif comm_sock in socks:
                 _, flag, message = comm_sock.recv_multipart()
+                self.logger.debug(f"Recieving reply to request: {flag}")
                 if flag == REP_CLIENT_NODE:
                     self.add_node(known_nodes, pickle.loads(message))
                     url_list = pending_recv
@@ -88,8 +94,11 @@ class ScrapChordClient:
                 pending_recv.add(url)
                 target_addr = self.select_target_node(url, known_nodes)
                 message = pickle.dumps((url, self.address))
-                connect_router(comm_sock, target_addr)
-                comm_sock.send_multipart([target_addr.encode(), message])
+                self.logger.debug(f"sending {url} to {target_addr}")
+                if not target_addr in connected:
+                    connect_router(comm_sock, target_addr)
+                    connected.add(target_addr)#time.sleep(1000000)
+                comm_sock.send_multipart([address_to_string(target_addr).encode(), message])
 
     def select_target_node(self, url, known_nodes) -> str:
         if len(known_nodes) == 0:
@@ -105,7 +114,7 @@ class ScrapChordClient:
         raise Exception("A node must be always found")
 
     def add_node(self, known_nodes:SortedSet, *nodes_addr):
-        for addr in known_nodes:
+        for addr in nodes_addr:
             addr_hash = f"{addr[0]}:{addr[1]}"
             idx = get_id(addr_hash) % (2 ** self.bits)
             known_nodes.add((idx, addr))
@@ -115,6 +124,7 @@ class ScrapChordClient:
             port=CHORD_BEACON_PORT,
             code_word=CODE_WORD_CHORD,
             tolerance=3,
-            all = 1
+            all = True
         )
+        self.logger.debug(f"Getting chord nodes: {chord_nodes}")
         return chord_nodes
