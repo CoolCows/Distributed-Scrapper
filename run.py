7bb3f6cecbe2dc22f9aaaecde782f0e6a72ce565
RodroVMS <democlesvanshepars@gmail.com>
2021-06-15 21:52:43 -0400

    Add run scrip for launching the different nodes of the scraper

---
+++ run.py
@@ -0,0 +1,29 @@
+import sys
+from scrap_chord import ScrapChordClient, ScrapChordNode
+from scraper import Scraper
+
+def main(node_type, args):
+    if node_type in {"chord", "scrapchord", "sc"}:
+        port = int(args[0])
+        m = int(args[1])
+        visible = False if len(args) == 2 and args[2] =="f" else True
+        sc = ScrapChordNode(port, m, visible)
+        sc.run()
+    
+    elif node_type in {"scraper", "scrapper", "s"}:
+        port = int(args[0])
+        max_t = int(args[1])
+        visible = False if len(args) == 2 and args[2] =="f" else True
+        s = Scraper(port, max_t, visible)
+        s.run()
+    
+    elif node_type in {"client", "cli", "c"}:
+        port = int(args[0])
+        m = int(args[1])
+        c = ScrapChordClient(port, m)
+        c.run()
+
+
+if __name__ == "__main__":
+    node_type, *args = sys.argv[1:]
+    main(node_type, args)
