4ecaf71c754c513cb0fac02e3d06d4e69e5f7e0e
RodroVMS <democlesvanshepars@gmail.com>
2021-06-21 14:18:46 -0400

    Fix run

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 import sys
 from scrap_chord import ScrapChordClient, ScrapChordNode
 from scraper import Scraper
@@ -6,17 +7,18 @@ def main(node_type, args):
     if node_type in {"chord", "scrapchord", "sc"}:
         port = int(args[0])
         m = int(args[1])
-        visible = int(args[2]) if len(args) > 3 else 8
-        forever = True if len(args) > 4 and args[3] == 'v' else False
+        join_ip = args[2] if len(args) > 3 else 0
+        visible = int(args[3]) if len(args) > 4 else 8
+        forever = False if len(args) > 5 and args[4] == 'f' else True
         sc = ScrapChordNode(port, m)
-        sc.run(visible=visible, forever=forever)
+        sc.run(addr=join_ip, visible=visible, forever=forever)
     
     elif node_type in {"scraper", "scrapper", "s"}:
         port = int(args[0])
         max_t = int(args[1])
         s = Scraper(port, max_t)
         visible = int(args[2]) if len(args) > 3 else 15
-        forever = True if len(args) > 4 and args[3] == 'v' else False
+        forever = False if len(args) > 4 and args[3] == 'f' else True
         s.run(visible=visible, forever=forever)
     
     elif node_type in {"client", "cli", "c"}:
