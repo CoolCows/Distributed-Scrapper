ba1718d73aa5dd4c9c5157e4a9943404dfda0b0b
RodroVMS <democlesvanshepars@gmail.com>
2021-06-20 22:54:52 -0400

    Change according to new changes in the aplication

--- run.py
+++ run.py
@@ -6,16 +6,18 @@ def main(node_type, args):
     if node_type in {"chord", "scrapchord", "sc"}:
         port = int(args[0])
         m = int(args[1])
-        visible = False if len(args) >= 3 and args[2] =="f" else True
-        sc = ScrapChordNode(port, m, visible=visible)
-        sc.run()
+        visible = int(args[2]) if len(args) > 3 else 8
+        forever = True if len(args) > 4 and args[3] == 'v' else False
+        sc = ScrapChordNode(port, m)
+        sc.run(visible=visible, forever=forever)
     
     elif node_type in {"scraper", "scrapper", "s"}:
         port = int(args[0])
         max_t = int(args[1])
-        visible = False if len(args) >= 3 and args[2] =="f" else True
-        s = Scraper(port, max_t, visible)
-        s.run()
+        s = Scraper(port, max_t)
+        visible = int(args[2]) if len(args) > 3 else 15
+        forever = True if len(args) > 4 and args[3] == 'v' else False
+        s.run(visible=visible, forever=forever)
     
     elif node_type in {"client", "cli", "c"}:
         port = int(args[0])
@@ -26,10 +28,11 @@ def main(node_type, args):
 
 
 if __name__ == "__main__":
-    try:
-        node_type, *args = sys.argv[1:]
-    except ValueError:
-        print("DEBUGING")
-        node_type = "sc"
-        args = ["7070", "5", "f"]#, "127.0.1.1:7050"]
+    node_type, *args = sys.argv[1:]
+    # try:
+    #     
+    # except ValueError:
+    #     print("DEBUGING")
+    #     node_type = "sc"
+    #     args = ["7070", "5", "f"]#, "127.0.1.1:7050"]
     main(node_type, args)
